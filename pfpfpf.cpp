6ad0aeddf4cfe4aa13d1b100182f4d6b228a10ae
zhzhzhzh77 <olzhabek.zh2007@gmail.com>
2025-02-25 23:02:55 +0600

    Add files via upload

---
+++ pfpfpf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    cout << "Hello, " << name << "!" << endl;
+
+    return 0;
+}
